7a16d4d25675873ec4ff26ddc1d72eda0bd6fbb4
Vicente T Freitas <vicenteteixeirafreitas@gmail.com>
2021-03-04 09:29:49 -0300

    Projeto criado.

---
+++ src/main/java/com/unimedceara/unimedcearareceita/UnimedCearaReceitaApplication.java
@@ -0,0 +1,13 @@
+package com.unimedceara.unimedcearareceita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimedCearaReceitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimedCearaReceitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unimedceara/unimedcearareceita/UnimedCearaReceitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimedceara.unimedcearareceita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnimedCearaReceitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
